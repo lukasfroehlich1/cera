85f8c5ff4328c6a5728d2e379e6894de6f053022
Tiange Wu <origami7795@gmail.com>
2016-04-23 12:08:16 -0700

    Added basic form starter

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+app.set('view engine', 'pug');
+
+app.set('views', __dirname+'/public/views');
+app.use('/', express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.render('index.pug');
 });
 
 app.listen(3000, function() {
