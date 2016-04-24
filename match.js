ad128c4e819f5e119bf1c92e387caf361a44d35e
Lukas Froehlich <lukasfroehlich1@gmail.com>
2016-04-23 20:12:39 -0700

    first write up of matching algo. still needs to be wrapped as a module and tested

--- match.js
+++ match.js
@@ -1,4 +1,5 @@
 var GoogleMapsAPI = require('googlemaps');
+var async = require('async');
 
 
 var publicConfig = {
@@ -10,24 +11,70 @@ var publicConfig = {
 
 var gmAPI = new GoogleMapsAPI(publicConfig);
 
+var test_drivers = [ { id: 1,
+  leave_earliest: Fri May 06 2016 00:00:00 GMT-0400 (EDT),
+  leave_latest: Sat May 07 2016 00:00:00 GMT-0400 (EDT),
+  waypoints: '41.40338, 2.17403|41.40338, 2.17403',
+  end_point: '61.40338, 3.17403',
+  start_point: "UCSD",
+  trip_time: 3600,
+  threshold: 1200,
+  price_seat: 20,
+  seats: 4 } ,
+ { id: 2,
+  leave_earliest: Fri May 06 2016 00:00:00 GMT-0400 (EDT),
+  leave_latest: Sat May 07 2016 00:00:00 GMT-0400 (EDT),
+  waypoints: '41.40338, 2.17403|41.40338, 2.17403',
+  end_point: '61.40338, 3.17403',
+  startId: 1,
+  trip_time: 3600,
+  threshold: 1200,
+  price_seat: 20,
+  seats: 4 } ]
 
-find_match = function() { // drivers, riders) {
-    gmAPI.directions({origin: "UCSD", destination: "SF", waypoints: "optimize:true|37.422, -122.084|36.5041, -121.9316"}, function(err, results) {
-        if (err) {
-            console.log('Error' + err);
-        }
-        else if (results['status'] == 'NOT_FOUND')
-		{
-			console.log("Location not found");
-			return results.status("404").send("Not found");
-		}
-        else {
-            console.log(results);
-            console.log(results.routes[0].legs[0].duration.value);
-        }
-	});
+
+find_match = function(driver, rider) {
+    var end_points = rider.end_points.split("|");
+	var result = false;
+    for (end_point in end_points) {
+		gmAPI.directions({origin: driver.start_point, destination: driver.end_point,
+			waypoints: "optimize:true|" + driver.waypoints + "|" + end_point}, function(err, results) {
+			if (err) {
+				console.log('Error' + err);
+			}
+			else {
+				console.log(results);
+				console.log(results.routes[0].legs[0].duration.value);
+
+				var new_trip_time = results.routes[0].legs[0].duration.value;
+
+				if (new_trip_time - driver.trip_time <= driver.threshold) {
+					result = {"driver_id": driver.id, "end_point": end_point, "new_trip_time": new_trip_time};
+				}
+			}
+		});
+		if (result) {
+			break;
+		}	
+	}
+	return result;
 }
 
-find_match();
 
+map_riders_to_drivers = function(riders, drivers){
+    var result;
+	async.map(riders, function(rider, callback1) { 
+		async.map(drivers, function(driver, callback2) {
+			callback2(null, find_match(rider, driver));
+		}, function(err, results) {
+			callback1(null, results.filter(function (x) { return x != false }));
+		});
+	}, function(err, results) {
+		if (err) {
+			console.log("Error: " + err);
+		}
+        result = results;
+	});
+    return result;
+};
 
