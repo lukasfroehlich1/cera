385f4e14ef7f5e707b9fe1de263be1aac1c0cc53
Lukas Froehlich <lukasfroehlich1@gmail.com>
2016-04-24 04:56:34 -0700

    put percolate adn connnect in the same file. reading for testing

--- connect.js
+++ connect.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 var GoogleMapsAPI = require('googlemaps');
 var async = require('async');
+var match = require('./match');
 var connect = module.exports = {};
 
 var con = mysql.createConnection({
@@ -241,4 +242,40 @@ connect.updateDriver = function (driverId, leaveDate, leaveEarliest, leaveLatest
         }
     });
 }
-connect.addDriver(2,"5 April, 2016","15:04","17:20","","UCLA",2,500,20,5);
+
+//connect.addDriver(2,"5 April, 2016","15:04","17:20","","UCLA",2,500,20,5);
+
+
+
+get_matches = function(riderId) {
+    con.query("SELECT * FROM `Match` WHERE id = ?", [riderId], function(err, rows) { 
+        if ( err ) throw err;
+        console.log("Requesting matches");
+        return rows;
+    });
+}
+
+update_matches = function() {
+    con.query("SELECT id, leave_date, leave_earliest, leave_latest, waypoints, end_point, coordinate start_point, trip_time, threshold from `Driver` join `ValidStarts` where startId = ValidStarts.id", [], function(err, drivers) {
+        if (err) throw err;
+
+
+        con.query("SELECT id, leave_date, leave_earliest, leave_latest, coordinate start_point, end_points from `Rider` join `ValidStarts` where startId = ValidStarts.id", [], function(err, riders) {
+
+            if (err) throw err;
+
+            con.query("Truncate table `Match`", [], function(err, riders) {
+
+                if (err) throw err;
+
+                match.map_riders_to_drivers(riders, drivers, function(results) {
+
+                    con.query("INSERT INTO `Match` values (?, ?, ?, ?)", [results.rider_id, results.driver_id, results.rider_end_point, results.new_trip_time], function(err, riders) {
+
+                        if (err) throw err;
+                    }
+                }
+            }
+        }
+    }
+}
