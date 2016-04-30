1d1c2ad6c7d919837fc243c8b5b5dbe6dfcd3708
Lukas Froehlich <lukasfroehlich1@gmail.com>
2016-04-30 11:14:50 -0700

    basic single tests are working. need to add in departure_day and tests for it

--- match.js
+++ match.js
@@ -3,52 +3,44 @@ var async = require('async');
 
 var match = module.exports = {};
 
-
 var publicConfig = {
-  key: 'AIzaSyB_1bO9S2BzI5TmiQDIIVT1G-9uMbVWUd8',
-  stagger_time:       1000, // for elevationPath
-  encode_polylines:   false,
-  secure:             true, // use https
+    key: 'AIzaSyB_1bO9S2BzI5TmiQDIIVT1G-9uMbVWUd8',
+    stagger_time:       1000, // for elevationPath
+    encode_polylines:   false,
+    secure:             true, // use https
 };
 
 var gmAPI = new GoogleMapsAPI(publicConfig);
 
-
 find_match = function(rider, driver, big_callback) {
-    console.log("endPoint: " + rider.end_points);
-    console.log(rider);
-    console.log(driver);
-
     var end_points = rider.end_points.split("|");
 
-    if (driver.start_point != rider.start_point || driver.leave_earliest > rider.leave_latest
-            || driver.leave_latest < rider.leave_earliest) {
-        big_callback(false);
+    if (driver.start_point != rider.start_point || driver.leave_earliest > rider.leave_latest || 
+            driver.leave_latest < rider.leave_earliest) {
+        big_callback(null, false);
     }
     else {
         var additional_time;
         var i = 0;
         async.doUntil(
             function(callback) {
-                console.log('here');
                 var leave_earliest = Math.max(driver.leave_earliest, rider.leave_earliest);
                 var leave_latest = Math.min(driver.leave_latest, rider.leave_latest);
-
 
                 gmAPI.directions({origin: driver.start_point, destination: driver.end_point,
                     waypoints: "optimize:true|" + (driver.waypoints + "|" + end_points[i]).split(' ').join('+'),
                     departure_time: (leave_earliest + leave_latest) / 2 
                 }, function(err, results) {
                     if (err) {
-                        console.log('Error :( -> ' + err);
+                        console.log('Error: ' + err);
                         console.log('most likely invalid location input');
-                    }else {
-                        var new_trip_time = results.routes[0].legs.map(function (x) { 
+                        i++;
+                        // not raising an error just quietly ignoring 
+                        callback(null, false);
+                    } else {
+                        var new_trip_time = results.routes[0].legs.map(function (x) {
                             return x.duration.value;
                         }).reduce(function (a, b) { return a + b; }, 0);
-
-                        console.log("new_trip_time " + new_trip_time);
-                        console.log("driver.trip_time" + driver.trip_time);
 
                         additional_time = new_trip_time - driver.trip_time;
 
@@ -60,45 +52,58 @@ find_match = function(rider, driver, big_callback) {
                                         "leave_latest": leave_latest});
                     }
                 }
-            )},
+            );},
             function() {
                 return additional_time <= driver.threshold || i == end_points.length;
             },
             function(err, results){
+                if (err) big_callback(null, false);
                 if (i == end_points.length && additional_time > driver.threshold) {
                     console.log("no match");
-                    big_callback(false);
+                    big_callback(null, false);
                 } else {
                     console.log("match");
-                    big_callback(results);
+                    big_callback(null, results);
                 }
             }
           );
     }
-}
+};
 
 // returns a list of [{rider_id, driver_id, rider_end_pint, new_trip_time, leave_earliest, leave_latest },
 //                    {rider_id, driver_id, rider_end_pint, new_trip_time, leave_earliest, leave_latest },
 //                    {rider_id, driver_id, rider_end_pint, new_trip_time, leave_earliest, leave_latest }] , one per rider
-match.map_riders_to_drivers = function(riders, drivers, callback){
-    console.log(riders);
-    console.log(drivers);
+match.map_riders_to_drivers = function(riders, drivers, callback) {
+    console.log("Trying to match riders and drivers...");
+    console.log("Matching " + riders.length + " rider(s) and " + drivers.length + " driver(s)"); 
+    //console.log(riders);
+    //console.log(drivers);
 
 	async.map(riders, function(rider, callback1) { 
-		async.map(drivers, function(driver, callback2) {
-            //console.log(rider);
-            find_match(rider, driver, function(results) {
+        async.map(drivers, function(driver, callback2) {
+            find_match(rider, driver, function(err, results) {
+                if (err) {
+                    callback2(err);
+                }
                 callback2(null, results);
-            })
-		}, function(err, results) {
-			callback1(null, results.filter(function (x) { return x != false }));
-		});
-	}, function(err, results) {
+            });
+        }, function(err, results) {
+            if (err) {
+                callback1(err);
+            }
+            callback1(null, results.filter(function (x) { return x !== false; }));
+        });
+    }, function(err, results) {
 		if (err) {
 			console.log("Error: " + err);
+            callback(err);
 		}
         else {
-            callback([].concat.apply([], results));
+            flat = [].concat.apply([], results);
+            //console.log("outputting flat");
+            //console.log(flat);
+            console.log("Found " + flat.length + " match(es)");
+            callback(null, flat);
         }
 	});
 };
--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 var match = require("../match");
 var assert = require('chai').assert;
 
-describe('Array', function() {
-	describe('#indexOf()', function () {
-		it('should return a valid response', function (done) {
+describe('Matching', function() {
+	describe('#validSingleMatch()', function () {
+		it('should match this simple case', function (done) {
 			var test_drivers = [ { id: 1,
 				leave_earliest: 100,
 				leave_latest: 200,
@@ -22,11 +22,110 @@ describe('Array', function() {
 				start_point: "UCLA,CA"
 			}];
 
-			match.map_riders_to_drivers(test_riders, test_drivers, function(res) {
-                if (err) throw err;
+			match.map_riders_to_drivers(test_riders, test_drivers, function(err, res) {
+                assert.lengthOf(res, 1, 'found one match');
+                done();
+            });
+		});
+		it('should match endpoints in provided', function (done) {
+			var test_drivers = [ { id: 1,
+				leave_earliest: 100,
+				leave_latest: 200,
+				waypoints: '',
+				end_point: 'Berkeley,CA',
+				start_point: "UCLA,CA",
+				trip_time: 19260,
+				threshold: 3600,
+				price_seat: 20,
+				seats: 4 } ];
 
-                asser.equal
-                
+			var test_riders = [ { id: 3,
+				leave_earliest: 150,
+				leave_latest: 160,
+				end_points: 'San Jose,CA|Berkeley,CA',
+				start_point: "UCLA,CA"
+			}];
+
+			match.map_riders_to_drivers(test_riders, test_drivers, function(err, res) {
+                assert.lengthOf(res, 1, 'found one match');
+                assert(res[0].rider_end_point == 'San Jose,CA',
+                        'should match with riders first choice even though the second choice takes less time');
+                done();
+            });
+		});
+		it('should ignore first two endpoints as they are invalid locations', function (done) {
+			var test_drivers = [ { id: 1,
+				leave_earliest: 100,
+				leave_latest: 200,
+				waypoints: '',
+				end_point: 'Berkeley,CA',
+				start_point: "UCLA,CA",
+				trip_time: 19260,
+				threshold: 3600,
+				price_seat: 20,
+				seats: 4 } ];
+
+			var test_riders = [ { id: 3,
+				leave_earliest: 150,
+				leave_latest: 160,
+				end_points: 'New York|doesntexist|San Mateo,CA',
+				start_point: "UCLA,CA"
+			}];
+
+			match.map_riders_to_drivers(test_riders, test_drivers, function(err, res) {
+                assert.lengthOf(res, 1, 'found one match');
+                assert(res[0].rider_end_point == 'San Mateo,CA', 'all other choices are invalid'); 
+                done();
+            });
+		});
+	});
+	describe('#invalidSingleMatch()', function () {
+		it('should return nothing. starting locations differ', function (done) {
+			var test_drivers = [ { id: 1,
+				leave_earliest: 100,
+				leave_latest: 200,
+				waypoints: '',
+				end_point: 'Berkeley,CA',
+				start_point: "UCSD,CA",
+				trip_time: 19260,
+				threshold: 3600,
+				price_seat: 20,
+				seats: 4 } ];
+
+			var test_riders = [ { id: 3,
+				leave_earliest: 150,
+				leave_latest: 160,
+				end_points: 'San Mateo,CA',
+				start_point: "UCLA,CA"
+			}];
+
+			match.map_riders_to_drivers(test_riders, test_drivers, function(err, res) {
+                assert.lengthOf(res, 0, 'no match found, different starting');
+                done();
+            });
+		});
+		it('should return nothing. times dont overlap locations differ', function (done) {
+			var test_drivers = [ { id: 1,
+				leave_earliest: 100,
+				leave_latest: 200,
+				waypoints: '',
+				end_point: 'Berkeley,CA',
+				start_point: "UCLA,CA",
+				trip_time: 19260,
+				threshold: 3600,
+				price_seat: 20,
+				seats: 4 } ];
+
+			var test_riders = [ { id: 3,
+				leave_earliest: 250,
+				leave_latest: 300,
+				end_points: 'San Mateo,CA',
+				start_point: "UCLA,CA"
+			}];
+
+			match.map_riders_to_drivers(test_riders, test_drivers, function(err, res) {
+                assert.lengthOf(res, 0, 'no match found. times do not overlap');
+                done();
             });
 		});
 	});
