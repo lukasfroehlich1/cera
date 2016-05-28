ca2b1c2b8db8a7ef207f533c6d89743b9e254858
calebhskim <me@calebkim.me>
2016-05-28 14:49:55 -0700

    Add tslint

--- app/tests/ts/MatchTest.ts
+++ app/tests/ts/MatchTest.ts
@@ -1,5 +1,5 @@
 /// <reference path="../../typings/globals/mocha/index.d.ts" />
-/// <reference path="../../typings/globals/chai/index.d.ts" />
+/// <reference path="../../typings/modules/chai/index.d.ts" />
 
 import {expect} from 'chai';
 import {map_riders_to_drivers} from "../../models/match";
